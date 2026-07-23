6508744d0ba070ca231b620a7c302143f1800846
Maxitoto <tommyush2000@hotmail.com>
2026-07-23 03:35:40 -0300

    fix: muchas peticiones de socios

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,15 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { lightTheme } from '#styles/theme'
 import App from './App.tsx'
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 1000 * 60 * 5, 
+      retry: 1,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
